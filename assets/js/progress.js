99b1bf323602281aeda3bf054dadb8da8d75ee59
Rahul Ranjan <rahul.ranjan@udacity.com>
2016-10-18 17:06:10 +0530

    remove console log statement as it was slowing rendering

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -2,7 +2,7 @@ $(window).load(function(){
   $(window).scroll(function() {
     var wintop = $(window).scrollTop(), docheight = $('.wrapper').height(), winheight = $(window).height();
     var totalScroll = (wintop/(docheight-winheight)) * 100;
-    console.log("total scroll" + totalScroll);
+    // console.log("total scroll" + totalScroll);
     $(".progress-bar").css("width", totalScroll + "%");
   });
 });
